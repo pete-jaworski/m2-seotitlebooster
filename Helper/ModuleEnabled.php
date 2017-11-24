203358d7371464d07b324fab05687c82a05c6615
Piotr Jaworski <piotrjaworski@hotmail.com>
2017-11-24 14:30:57 +0100

    Completed ModuleEnable class

--- Helper/ModuleEnabled.php
+++ Helper/ModuleEnabled.php
@@ -8,8 +8,36 @@ namespace PiotrJaworski\SEOTitleBooster\Helper;
  */
 class ModuleEnabled
 {
+    /**
+     * Holds module enable config name
+     */
+    const MODULE_CONFIG_NAME = 'piotrjaworski_seotitlebooster/general/seotitlebooster_enabled';
+
+    
+    /**
+     *
+     * @var \Magento\Framework\App\Config\ScopeConfigInterface 
+     */
+    protected $scopeConfig;
+    
+    
+    /**
+     * Constructor 
+     * 
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     */
+    public function __construct(\Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig)
+    {
+        $this->scopeConfig = $scopeConfig;
+    }
+    
+    
+    /**
+     * Checks if module is enabled
+     * @return bool
+     */
     public function enabled()
     {
-        return false; 
+        return (bool)$this->scopeConfig->getValue(self::MODULE_CONFIG_NAME, \Magento\Store\Model\ScopeInterface::SCOPE_STORE); 
     }
 }
